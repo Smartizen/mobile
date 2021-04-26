d7016855b57e46d1b6cffc9abf3412780e67cfda
hoang <vinhyenvodoi98@gmail.com>
2021-04-26 20:46:19 +0700

    chore : change devices field to actives

--- lib/Models/rooms.dart
+++ lib/Models/rooms.dart
@@ -1,19 +1,17 @@
-import 'package:smartizen/Models/devices.dart';
-
 class Rooms {
   String id;
   String name;
-  List<Devices> devices;
+  List<Actives> actives;
 
-  Rooms({this.id, this.name, this.devices});
+  Rooms({this.id, this.name, this.actives});
 
   Rooms.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
-    if (json['devices'] != null) {
-      devices = new List<Devices>();
-      json['devices'].forEach((v) {
-        devices.add(new Devices.fromJson(v));
+    if (json['actives'] != null) {
+      actives = new List<Actives>();
+      json['actives'].forEach((v) {
+        actives.add(new Actives.fromJson(v));
       });
     }
   }
@@ -22,14 +20,35 @@ class Rooms {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
     data['name'] = this.name;
-    if (this.devices != null) {
-      data['devices'] = this.devices.map((v) => v.toJson()).toList();
+    if (this.actives != null) {
+      data['actives'] = this.actives.map((v) => v.toJson()).toList();
     }
     return data;
   }
 
   @override
   String toString() {
-    return 'Rooms: {id: $id, name: $name,devices: $devices}';
+    return 'Rooms: {id: $id, name: $name,actives: $actives}';
+  }
+}
+
+class Actives {
+  String deviceId;
+
+  Actives({this.deviceId});
+
+  Actives.fromJson(Map<String, dynamic> json) {
+    deviceId = json['deviceId'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['deviceId'] = this.deviceId;
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'Actives : {deviceId: $deviceId}';
   }
 }
